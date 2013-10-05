1db0a8d9a7faf87d700d14e800c9c07584f502ea
Falko Schumann <falko.schumann@muspellheim.de>
2013-10-05 22:03:33 +0200

    Checkstyle aktiviert.
    ... und die meisten Warnungen beseitigt.

--- src/main/java/de/bsvrz/iav/fuzzylib/fuzzylib/LinguistischerTerm.java
+++ src/main/java/de/bsvrz/iav/fuzzylib/fuzzylib/LinguistischerTerm.java
@@ -1,3 +1,30 @@
+/*
+ * Funktionen Fuzzy, Intelligente Analyseverfahren (IAV)
+ * Copyright (c) 2013, Falko Schumann <http://www.muspellheim.de>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *   - Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *   - Redistributions in binary form must reproduce the above copyright notice,
+ *     this list of conditions and the following disclaimer in the documentation
+ *     and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package de.bsvrz.iav.fuzzylib.fuzzylib;
 
 /**
@@ -37,7 +64,8 @@ public class LinguistischerTerm {
 
     public void setZugehoerigkeit(double zugehoerigkeit) {
         if (zugehoerigkeit < 0 || zugehoerigkeit > 1)
-            throw new IllegalArgumentException("Die Zugehörigkeit muss im Intervall [0,1] liegen: " + zugehoerigkeit);
+            throw new IllegalArgumentException("Die Zugehörigkeit muss im Intervall [0,1] liegen: "
+                    + zugehoerigkeit);
         this.zugehoerigkeit = zugehoerigkeit;
     }
 
@@ -48,7 +76,8 @@ public class LinguistischerTerm {
         } else if (obj instanceof LinguistischerTerm) {
             LinguistischerTerm other = (LinguistischerTerm) obj;
             boolean result = true;
-            result &= (name == null && other.name == null) || (name != null && name.equals(other.name));
+            result &= (name == null && other.name == null)
+                    || (name != null && name.equals(other.name));
             result &= Double.compare(zugehoerigkeit, other.zugehoerigkeit) == 0;
             return result;
         }
